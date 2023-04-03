394f1d77f4941c377b7b06ea2812d96880bfaf09
LeonardoMdSACode <leonar_ansari_8@hotmail.com>
2023-04-04 00:36:18 +0100

    lalala

---
+++ CNN_flowers_data_augmentation.py
@@ -0,0 +1,43 @@
+# Image Classification CNN, with Data Augmentation tecniques
+import os
+import numpy as np
+import glob
+import shutil
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Data Loading
+print(("Data Loading"))
+_URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+zip_file = tf.keras.utils.get_file(origin=_URL,
+                                   fname="flower_photos.tgz",
+                                   extract=True)
+
+base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
+
+# Creating labels
+classes = ['roses', 'daisy', 'dandelion', 'sunflowers', 'tulips']
+
+# prints the total number of flowers imgs for each type
+for cl in classes:
+   img_path = os.path.join(base_dir, cl)
+   images = glob.glob(img_path + '/*.jpg')
+   print("{}: {} Images".format(cl, len(images)))
+   num_train = int(round(len(images)*0.8))
+   train, val = images[:num_train], images[num_train:]
+
+   for t in train:
+      if not os.path.exists(os.path.join(base_dir, 'train', cl)):
+         os.makedirs(os.path.join(base_dir, 'train', cl))
+      shutil.move(t, os.path.join(base_dir, 'train', cl))
+
+   for v in val:
+      if not os.path.exists(os.path.join(base_dir, 'val', cl)):
+         os.makedirs(os.path.join(base_dir, 'val', cl))
+      shutil.move(v, os.path.join(base_dir, 'val', cl))
+
+round(len(images)*0.8)
--- catsdogs_CNN_imgAugm_Dropout.py
+++ catsdogs_CNN_imgAugm_Dropout.py
@@ -1,6 +1,9 @@
 # CNN of cats and dogs classification, images have colors and diferent sizes
+# APPLYING IMAGE AUGMENTATION AND DROPOUT TO INCREASE ACCURACY AND DECREASE VALIDATION LOSS
 from __future__ import absolute_import, division, print_function
 import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -32,7 +35,7 @@ num_dogs_val = len(os.listdir(validation_dogs_dir))
 total_train = num_cats_tr + num_dogs_tr
 total_val = num_cats_val + num_dogs_val
 
-print('total training cat images: ', num_cats_tr)
+print("total training cat images: ", num_cats_tr)
 print("total training dog images: ", num_dogs_tr)
 print("total validation cat images: ", num_cats_val)
 print("total validation dog images: ", num_dogs_val)
@@ -44,24 +47,76 @@ print("Parameters")
 BATCH_SIZE = 100  # Number of examples processed b4 updating model variables thro feedback loop
 IMG_SHAPE = 150  # 150x150 pixels
 
-# Data Prep
-print("Data prep")
-print("ImageDataGenerator")
-print(".flow_from_directory")
-print("train and validation datasets")
-train_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator training data
-validation_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator validation data
+# DATA AUGMENTATION
+print("Data Augmentation")
 
-train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
-                                                           directory=train_dir,
-                                                           shuffle=True,
-                                                           target_size=(IMG_SHAPE, IMG_SHAPE),  # (150,150)
-                                                           class_mode='binary')
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
-                                                              directory=validation_dir,
-                                                              shuffle=False,
-                                                              target_size=(IMG_SHAPE, IMG_SHAPE),  # (150,150)
-                                                              class_mode='binary')
+
+# function to plot imgs
+def plotImages(images_arr):
+   fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+   axes = axes.flatten()
+   for img, ax in zip(images_arr, axes):
+      ax.imshow(img)
+   plt.tight_layout()
+   plt.show()
+
+
+# Flipping the image horizontally
+image_gen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
+train_data_gen = image_gen.flow_from_directory(batch_size=BATCH_SIZE,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE))
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+print("Flipping imgs")
+
+# Rotating the image
+image_gen = ImageDataGenerator(rescale=1. / 255, rotation_range=45)
+train_data_gen = image_gen.flow_from_directory(batch_size=BATCH_SIZE,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE))
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+print("Rotated imgs")
+
+# Applying Zoom
+image_gen = ImageDataGenerator(rescale=1. / 255, zoom_range=0.5)
+train_data_gen = image_gen.flow_from_directory(batch_size=BATCH_SIZE,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE))
+augmented_images =  [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+print("Zoom Applied")
+
+# Join everything
+image_gen_train = ImageDataGenerator(
+   rescale=1. / 255,
+   rotation_range=40,
+   width_shift_range=0.2,
+   height_shift_range=0.2,
+   shear_range=0.2,
+   zoom_range=0.2,
+   horizontal_flip=True,
+   fill_mode='nearest')
+train_data_gen = image_gen_train.flow_from_directory(batch_size=BATCH_SIZE,
+                                                     directory=train_dir,
+                                                     shuffle=True,
+                                                     target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                     class_mode='binary')
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+
+# Validation Dataset Generator
+image_gen_val = ImageDataGenerator(rescale=1. / 255)
+val_data_gen = image_gen_val.flow_from_directory(batch_size=BATCH_SIZE,
+                                                 directory=validation_dir,
+                                                 target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                 class_mode='binary')
+
 # Define Model
 print("CNN")
 print(".Sequential")
@@ -78,6 +133,7 @@ model = tf.keras.models.Sequential([
    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
    tf.keras.layers.MaxPooling2D(2, 2),
 
+   tf.keras.layers.Dropout(0.5),
    tf.keras.layers.Flatten(),
    tf.keras.layers.Dense(512, activation='relu'),
    tf.keras.layers.Dense(2, activation='softmax')
@@ -94,7 +150,7 @@ model.summary()
 
 print("Training Model:")
 print(".fit")
-EPOCHS = 7
+EPOCHS = 15
 history = model.fit(
    train_data_gen,
    steps_per_epoch=int(np.ceil(total_train / float(BATCH_SIZE))),
@@ -104,4 +160,5 @@ history = model.fit(
 )
 
 print("Validation Accuracy:", history.history['val_accuracy'][-1])
-# 7=0.727
+# 15=0.682
+
